2d3f357952a2c87a0a3d429c4f8ba412531aca7b
Davi Farhi <davifah@homelinux.mint>
2019-04-16 11:39:43 -0300

    Removed useless code

--- interface/video.js
+++ interface/video.js
@@ -35,26 +35,3 @@ function buildVideo(videoID){
     }
     $('#warn-webcam').text("Cameras: "+videoID.length);
 }
-
-
-/*window.onload = () => {
-//    let canvas = document.getElementById('canvas');
-//    let context = canvas.getContext('2d');
-    let video = document.getElementById('video');
-    let vendorUrl = window.URL || window.webkitURL;
-
-    navigator.getMedia =    navigator.getUserMedia ||
-                            navigator.webkitGetUserMedia ||
-                            navigator.mozGetUserMedia ||
-                            navigator.msGetUserMedia;
-
-    navigator.getMedia({
-        video: true,
-        audio: false
-    }, (stream) => {
-        video.srcObject = stream;
-        video.play();
-    }, (error) => {
-        console.log(error);
-    });
-};*/
