9179858b8937cf049a975be7285cac8990b7d68a
Davi Farhi <davifah@homelinux.mint>
2019-04-20 00:14:33 -0300

    Melhoria no tickrate:  -apscheduler

--- RPi/clienteRPi.py
+++ RPi/clienteRPi.py
@@ -1,22 +1,43 @@
 import requests
 # biblioteca responsável por acessar o servidor web (2.18.4)
-from apscheduler.schedulers.background import BackgroundScheduler
-# biblioteca responsável por rodar a função principal (3.6)
 import serial
 # biblioteca responsável por estabelecer a comunicaçao com o Arduino (3.4)
 import json
 from gpiozero import CPUTemperature
 # biblioteca para monitorar temperatura da cpu
+from classes.tempo import Tempo
 
 # ============ Variáveis =============
-ticks = 7
 URL = 'http://192.168.0.17:8080'
 arduinoPort = '/dev/ttyS0'
 usuario = 'RPi'
 baudrate = 200000
 
-# ========= Função Background ========
+# =========== Instâncias =========
 
+# Instância da porta Serial para comunicação com Arduino
+ser = serial.Serial(
+    port=arduinoPort,
+    baudrate=baudrate
+)
+# Instância para medir temperatura da CPU
+cpu = CPUTemperature()
+# Instância para pegar tickrate
+tmp = Tempo()
+
+# Dicionário (do JS: Objeto) de dados
+dadosPost = {
+    "usuario": usuario,
+    "dados": {
+        "temperatura": 0.00,
+        "arduino": False,
+        "potenciometro": None,
+        "ticks": 0
+    }
+
+}
+
+# ========= Funções ============
 
 def main():
     dadosOutput = dadosPost
@@ -33,43 +54,16 @@ def main():
     arduinoInput = json.loads(readSerial) #json.decoder.JSONDecodeError
 
     dadosOutput["dados"]["potenciometro"] = int(arduinoInput["potenciometro"])
-
     dadosOutput["dados"]["temperatura"] = round(cpu.temperature, 1)
+    dadosOutput["dados"]["ticks"] = tmp.setTicks()
 
     requests.post(URL, json=dadosOutput)
 
-# ======= Criação de loop de intervalo
+# ========= LOOP ========
 
+try:
+    while True:
+        main()
 
-if __name__ == '__main__':
-
-    # =========== Instâncias =========
-    ser = serial.Serial(
-        port=arduinoPort,
-        baudrate=baudrate,
-        timeout=2/ticks
-    )
-    # cria uma instância para porta serial
-    sched = BackgroundScheduler(
-        {'apscheduler.job_defaults.max_instances': '10'})
-    # cria uma instância para loop de intervalo
-    cpu = CPUTemperature()
-    sched.add_job(main, 'interval', seconds=1/ticks)
-    sched.start()
-
-    dadosPost = {
-        "usuario": usuario,
-        "dados": {
-            "temperatura": 0.00,
-            "arduino": False,
-            "potenciometro": None,
-        }
-    }
-
-    # =========== Comandos "setup" =====
-    try:
-        while True:
-            pass
-
-    except (KeyboardInterrupt, SystemExit):
-        sched.shutdown()
+except (KeyboardInterrupt, SystemExit):
+    print('Encerrando o programa')
